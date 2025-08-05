10a351a9fcf4f1715203cd82fd93d743f2ad82cb
tayyab-004 <tayyabgee333@gmail.com>
2025-08-05 15:11:10 +0500

    Styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, est.
+        </h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, iure
+        provident voluptatum enim voluptatibus porro ducimus id at delectus
+        illo!
+      </Link>
+    </div>
+  );
 }
