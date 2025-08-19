da5b5eb5012509fbe3e08ac1b883352c880e2dd5
tayyab-004 <tayyabgee333@gmail.com>
2025-08-19 22:14:32 +0500

    Bug Fixes

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -5,10 +5,21 @@ import styles from "./write.module.css";
 import Image from "next/image";
 import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.bubble.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const WritePage = () => {
+  const { status } = useSession();
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
 
   return (
     <div className={styles.container}>
